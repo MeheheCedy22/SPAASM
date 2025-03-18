98ef46dd95f0963f8735b39c0155fb855e1130e9
Marek Čederle <96355171+MeheheCedy22@users.noreply.github.com>
2025-03-18 13:05:34 +0100

    added some stuff

--- 1zadanie/main.asm
+++ 1zadanie/main.asm
@@ -1,10 +1,13 @@
 BUFFSIZE EQU 64000 ; input buffer size
 
 DATA SEGMENT
-    HELP1 DB 'Author: Marek Cederle','$'
+    HELP DB 'Author: Marek Cederle',10,13, 'Usage: MAIN.EXE [options] [files]',10,13, 'Options:',10,13, '  -h    Display this help message',10,13, '  -r    Display contents in reverse order',10,13, 'Files:',10,13, '  Specify one or more files to process.',10,13, 'Example: MAIN.EXE -r INPUT.TXT INPUT2.TXT','$'
     FILENAME DB 'INPUT.TXT', 0 ; define input file name (max 8 chars)
     BUFF DB BUFFSIZE DUP ('$') ; allocate buffer for input data
     ; ESTR DB ‚ERROR OPENNING FILE$' ; error message if file open fails
+    TEST_STR_NO_ARGS DB 'TEST_STRING_NO_ARGS', '$' ; test string
+    TEST_STR_REVERSE DB 'TEST_STR_REVERSE', '$' ; test string 2
+    ARGS_ERROR_MSG DB 'Invalid arguments. Use -h for help.', '$'
 DATA ENDS
 
 CODE SEGMENT
@@ -13,7 +16,7 @@ CODE SEGMENT
 ASSUME CS:CODE, DS:DATA ; init CS, DS (assigning the beginnings of segments to individual segment registers)
 
 ; clear screen procedure
-CLSREEN PROC
+CLSCREEN PROC
     MOV AX, 0003H ; alternativa AH,0 AL,03 ; sluzba videoBIOS
     INT 10H ; prerusenie BIOS
     RET
@@ -45,12 +48,12 @@ O_FILE MACRO FILENAME
     INT 21H
 ENDM ; pozor!! po skončení je v AX file_handle
 
-; TODO: DAVA ZATIAL ERROR
 ; close file
-; C_FILE:
-;     MOV AH, 3EH ; služba MS-DOS
-;     MOV BX, FILENAME ; v BX služba MS-DOS 3EH očakáva file_handle
-;     INT 21H
+C_FILE MACRO file_handle
+    MOV AH, 3EH ; služba MS-DOS
+    MOV BX, file_handle ; v BX služba MS-DOS 3EH očakáva file_handle
+    INT 21H
+ENDM
 
 ; print file
 P_FILE MACRO FILENAME ; v dat. segmente je - FILENAME DB 'SUBOR.TXT', 0
@@ -67,16 +70,83 @@ P_FILE MACRO FILENAME ; v dat. segmente je - FILENAME DB 'SUBOR.TXT', 0
 ; R_FILE
 
 ; print help
-P_HELP:
+P_HELP MACRO
+    MOV DX, OFFSET HELP
+    MOV AH, 09H
+    INT 21H
+ENDM
+
+; handle command-line arguments
+HANDLE_ARGS PROC
+    ; Check for '-h' argument to display help
+    MOV AX, 0 ; Initialize AX to zero
+    MOV SI, 80H ; Command-line arguments start at 80h in PSP
+    MOV CX, [SI] ; Get the length of the command-line arguments
+    CMP CX, 0
+    JE NO_ARGS ; If no arguments are present, jump to NO_ARGS
+    CALL SKIP_WHITESPACE
+    CMP BYTE PTR [SI], '-' ; Check for '-'
+    JNE NO_ARGS ; If not found, jump to NO_ARGS
+    INC SI ; Move to the next character
+    CMP BYTE PTR [SI], 'h' ; Check for 'h'
+    JE ARGS_HELP
+    CMP BYTE PTR [SI], 'r' ; Check for 'r'
+    JE ARGS_REVERSE
+    JMP ARGS_ERR ; If no valid arguments are found, jump to ARGS_ERR
+
+    ARGS_ERR:
+        W_STR ARGS_ERROR_MSG ; Display an error message
+        JMP EXIT ; Exit the program
+
+    ARGS_HELP:
+        P_HELP ; Display the help message
+        JMP EXIT ; Exit the program
+
+    ARGS_REVERSE:
+        ; Handle reverse order argument
+        ; ...
+        W_STR TEST_STR_REVERSE
+        JMP ARGS_END
+
+    NO_ARGS: 
+        ; Default behavior with files only
+        ; ...
+        W_STR TEST_STR_NO_ARGS
+        JMP ARGS_END
+
+    ARGS_END:
+        RET
+ENDP
+
+SKIP_WHITESPACE PROC
+    MOV AL, [SI] ; Load the current character into AL
+    WHITESPACE_LOOP:
+        CMP AL, ' ' ; Check if the character is a space
+        JE SKIP_CHAR ; If yes, skip it
+        CMP AL, 9 ; Check if the character is a tab
+        JE SKIP_CHAR ; If yes, skip it
+        CMP AL, 0DH ; Check if the character is a carriage return
+        JE SKIP_CHAR ; If yes, skip it
+        CMP AL, 0AH ; Check if the character is a newline
+        JE SKIP_CHAR ; If yes, skip it
+        JMP END_SKIP ; If no more whitespace, exit the loop
+
+    SKIP_CHAR:
+        INC SI ; Move to the next character
+        MOV AL, [SI] ; Load the next character into AL
+        JMP WHITESPACE_LOOP ; Repeat the loop
+
+    END_SKIP:
+        RET
+ENDP
 
 START:
-    MOV AX,DATA
-    MOV DS,AX
-    MOV AH,9
-    MOV DX, OFFSET HELP1
+    MOV AX,DATA             ; Move data segment address to AX
+    MOV DS,AX               ; Move data segment address to DS
+    CALL HANDLE_ARGS
+EXIT:
+    MOV AH,4CH ; DOS function to terminate the program
     INT 21H
-    MOV AH,4CH
-    INT 21H        
 CODE ENDS
 END START
 
