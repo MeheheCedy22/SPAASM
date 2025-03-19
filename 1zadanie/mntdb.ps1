9362458b54f61b70c677aefa6fe42ed6ed7068c0
Marek Čederle <96355171+MeheheCedy22@users.noreply.github.com>
2025-03-19 21:57:28 +0100

    replace .bat with .ps1

---
+++ 1zadanie/mntdb.ps1
@@ -0,0 +1,8 @@
+$dosBoxExe = "C:\Marek\Documents\College\6semester\SPAASM\Materialy_k_cviceniam_I\Materialy\TASM\Tasm64\Tasm 1.4\TawareSagar\DOSBox.exe"
+$arguments = @(
+    '-c', 'mount C .',
+    '-c', 'C:',
+    '-c', 'cd 1ZADANIE',
+    '-c', 'build.bat'
+)
+& $dosBoxExe @arguments
